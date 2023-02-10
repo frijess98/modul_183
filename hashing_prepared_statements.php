961716dd8fd657f5b9c5eb7240ef74427c491b31
Matthias <47850805+satansgrillmaster@users.noreply.github.com>
2023-02-10 13:53:06 +0100

    add prepare statement

--- hashing_prepared_statements.php
+++ hashing_prepared_statements.php
@@ -4,16 +4,43 @@
 
 
 // username & password
-$username = '';
-$password = '';
+$password = 'test';
 $passwordhash = '';
 
 // hash the password
-// param: password: string
-// param: algo: string
 
 $passwordhash = password_hash($password, PASSWORD_DEFAULT);
 
 // print the hash
 print($passwordhash);
+
+// prepared statements
+
+// https://www.w3schools.com/php/php_mysql_prepared_statements.asp
+
+$servername = 'localhost';
+$db_user_name = 'root';
+$db_password = '';
+$dbname = 'database.db';
+
+$first_name = 'Matthias';
+$password = '';
+
+
+// Create connection
+$conn = new mysqli($servername,$db_user_name,$db_password,$dbname);
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO users (first_name, PASSWORD(password)) VALUES (?, ?)");
+
+// bind param
+$stmt->bind_param("ss", $first_name, $password);
+
+// execute statement
+$stmt->execute();
+
+$stmt->close();
+$conn->close();
+
+?>
 
